d0231058736a6954a719458ff5077c122544ec58
ynn <ynn@mel.yt>
2017-11-08 11:26:45 +0100

    makes it less complex

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM golang:alpine
-COPY proxy.go /app/
-COPY run.sh /run.sh
-EXPOSE 8080
-CMD ["/run.sh"]
--- proxy.go
+++ proxy.go
@@ -5,7 +5,6 @@ import (
 	"io/ioutil"
 	"net/http"
 	"encoding/xml"
-	"strconv"
 	"time"
 	"os"
 )
@@ -16,7 +15,6 @@ type ObixValue struct {
 	Display string `xml:"display,attr"`
 }
 
-
 func handler(w http.ResponseWriter, r *http.Request) {
 	url := os.Getenv("OBIX_SERVER_URL") + r.URL.Path[1:]
 	resp, err := http.Get(url)
@@ -25,16 +23,12 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	if(resp.StatusCode!=http.StatusOK){
-		w.WriteHeader(resp.StatusCode)
-		return
-	}
 
 	extractName :=r.URL.Query().Get("extract")
 	format := r.URL.Query().Get("format")
 	serverName:=os.Getenv("OBIX_NAME");
 
-	if(format == ""){
+	if format == "" {
 		format = r.Header.Get("Accept")
 	}
 
@@ -47,58 +41,39 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	v := ObixValue{}
 	err = xml.Unmarshal(body, &v)
 
-	if err != nil {
+	if resp.StatusCode!=http.StatusOK {
+		w.WriteHeader(resp.StatusCode)
+		fmt.Fprintf(w, "%s", body)
 		return
 	}
 
-	if(v.XMLName.Local == "err"){
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+
+	if v.XMLName.Local == "err" {
 		http.Error(w, v.Display, http.StatusInternalServerError)
 		return
 	}
 
-	if extractName != "" || format == "text" {
-		switch format {
-		case "line", "application/line", "application/influxdb-line":
-			w.Header().Set("Content-type", "text/plain")
-			timestamp := time.Now().UnixNano()
-
-			switch v.XMLName.Local {
-			case "int":
-				if value, err := strconv.Atoi(v.Value); err == nil {
-					fmt.Fprintf(w, "%s %s=%d %d",serverName, extractName, value, timestamp)
-				}
-			case "real":
-				if value, err := strconv.ParseFloat(v.Value, 64); err == nil {
-					fmt.Fprintf(w, "%s %s=%f %d",serverName, extractName, value, timestamp)
-				}
-			default:
-				fmt.Fprintf(w, "%s %s=%s %d",serverName, extractName, v.Value, timestamp)
-			}
-		case "text","text/plain":
-			w.Header().Set("Content-type", "text/plain")
-			fmt.Fprintf(w, "%s", v.Value)
-		case "json", "application/json":
-			w.Header().Set("Content-type", "application/json")
-			switch v.XMLName.Local {
-			case "int":
-				if value, err := strconv.Atoi(v.Value); err == nil {
-					fmt.Fprintf(w, "{\"%s\" : %d}", extractName, value)
-				}
-			case "real":
-				if value, err := strconv.ParseFloat(v.Value, 64); err == nil {
-					fmt.Fprintf(w, "{\"%s\" : %f}", extractName, value)
-				}
-			default:
-				fmt.Fprintf(w, "{\"%s\" : \"%s\"}", extractName, v.Value)
-			}
-		default:
-			w.Header().Set("Content-type", "application/xml")
-			fmt.Fprintf(w, "%s", body)
-		}
-	}else{
+	switch format {
+	case "line", "application/line", "application/influxdb-line":
+		w.Header().Set("Content-type", "text/plain")
+		timestamp := time.Now().UnixNano()
+		fmt.Fprintf(w, "%s %s=%s %d",serverName, extractName, v.Value, timestamp)
+	case "text","text/plain":
+		w.Header().Set("Content-type", "text/plain")
+		fmt.Fprintf(w, "%s", v.Value)
+	case "json", "application/json":
+		w.Header().Set("Content-type", "application/json")
+		fmt.Fprintf(w, "{\"%s\" : %s}", extractName, v.Value)
+	default:
 		w.Header().Set("Content-type", "application/xml")
 		fmt.Fprintf(w, "%s", body)
 	}
+
 
 }
 
